46291afda1b464bf4bb9f787b5342d2f970019f3
tfjoek <125345194+tfjoek@users.noreply.github.com>
2024-10-08 17:26:56 +0300

    Update testi1.sql

--- testi1.sql
+++ testi1.sql
@@ -1,29 +1,26 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.5.2-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: flight_game
--- ------------------------------------------------------
--- Server version	11.5.2-MariaDB
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.5.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.6.0.6765
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `airport`
---
 
-DROP TABLE IF EXISTS `airport`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `airport` (
+-- Dumping database structure for flight_game
+CREATE DATABASE IF NOT EXISTS `flight_game` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+USE `flight_game`;
+
+-- Dumping structure for table flight_game.airport
+CREATE TABLE IF NOT EXISTS `airport` (
   `id` int(11) NOT NULL,
   `ident` varchar(40) NOT NULL,
   `name` varchar(40) DEFAULT NULL,
@@ -33,44 +30,30 @@ CREATE TABLE `airport` (
   `owner` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`ident`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `airport`
---
+-- Dumping data for table flight_game.airport: ~18 rows (approximately)
+REPLACE INTO `airport` (`id`, `ident`, `name`, `latitude_deg`, `longitude_deg`, `difficulty`, `owner`) VALUES
+	(2307, 'EFHK', 'Helsinki Vantaa Airport', 60.3172, 24.963301, 5, 'Finland'),
+	(2314, 'EFIV', 'Ivalo Airport', 68.607299804688, 27.405300140381, 3, 'Russia'),
+	(2315, 'EFJO', 'Joensuu Airport', 62.662899, 29.6075, 2, 'Russia'),
+	(2316, 'EFJY', 'Jyväskylä Airport', 62.399502, 25.678301, 1, 'Russia'),
+	(2318, 'EFKE', 'Kemi-Tornio Airport', 65.778701782227, 24.582099914551, 1, 'Russia'),
+	(2319, 'EFKI', 'Kajaani Airport', 64.2855, 27.6924, 2, 'Russia'),
+	(2321, 'EFKK', 'Kokkola-Pietarsaari Airport', 63.721199, 23.143101, 2, 'Russia'),
+	(2324, 'EFKS', 'Kuusamo Airport', 65.987602, 29.239401, 1, 'Russia'),
+	(2325, 'EFKT', 'Kittilä Airport', 67.700996398926, 24.846799850464, 1, 'Russia'),
+	(2326, 'EFKU', 'Kuopio Airport', 63.007099, 27.7978, 2, 'Russia'),
+	(2330, 'EFMA', 'Mariehamn Airport', 60.1222, 19.898199, 1, 'Finland'),
+	(2334, 'EFOU', 'Oulu Airport', 64.930099, 25.354601, 4, 'Russia'),
+	(2336, 'EFPO', 'Pori Airport', 61.4617, 21.799999, 1, 'Finland'),
+	(2341, 'EFRO', 'Rovaniemi Airport', 66.564796447754, 25.830400466919, 3, 'Russia'),
+	(2343, 'EFSA', 'Savonlinna Airport', 61.9431, 28.945101, 1, 'Russia'),
+	(2347, 'EFTP', 'Tampere-Pirkkala Airport', 61.414101, 23.604401, 3, 'Finland'),
+	(2349, 'EFTU', 'Turku Airport', 60.514099, 22.2628, 4, 'Finland'),
+	(2351, 'EFVA', 'Vaasa Airport', 63.050701, 21.762199, 1, 'Russia');
 
-LOCK TABLES `airport` WRITE;
-/*!40000 ALTER TABLE `airport` DISABLE KEYS */;
-INSERT INTO `airport` VALUES
-(2307,'EFHK','Helsinki Vantaa Airport',60.3172,24.963301,1,'Finland'),
-(2314,'EFIV','Ivalo Airport',68.607299804688,27.405300140381,1,'Russia'),
-(2315,'EFJO','Joensuu Airport',62.662899,29.6075,1,'Russia'),
-(2316,'EFJY','Jyväskylä Airport',62.399502,25.678301,1,'Russia'),
-(2318,'EFKE','Kemi-Tornio Airport',65.778701782227,24.582099914551,1,'Russia'),
-(2319,'EFKI','Kajaani Airport',64.2855,27.6924,1,'Russia'),
-(2321,'EFKK','Kokkola-Pietarsaari Airport',63.721199,23.143101,1,'Russia'),
-(2324,'EFKS','Kuusamo Airport',65.987602,29.239401,1,'Russia'),
-(2325,'EFKT','Kittilä Airport',67.700996398926,24.846799850464,1,'Russia'),
-(2326,'EFKU','Kuopio Airport',63.007099,27.7978,1,'Russia'),
-(2330,'EFMA','Mariehamn Airport',60.1222,19.898199,1,'Russia'),
-(2334,'EFOU','Oulu Airport',64.930099,25.354601,1,'Russia'),
-(2336,'EFPO','Pori Airport',61.4617,21.799999,1,'Russia'),
-(2341,'EFRO','Rovaniemi Airport',66.564796447754,25.830400466919,1,'Russia'),
-(2343,'EFSA','Savonlinna Airport',61.9431,28.945101,1,'Russia'),
-(2347,'EFTP','Tampere-Pirkkala Airport',61.414101,23.604401,1,'Russia'),
-(2349,'EFTU','Turku Airport',60.514099,22.2628,1,'Russia'),
-(2351,'EFVA','Vaasa Airport',63.050701,21.762199,1,'Russia');
-/*!40000 ALTER TABLE `airport` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `game`
---
-
-DROP TABLE IF EXISTS `game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game` (
+-- Dumping structure for table flight_game.game
+CREATE TABLE IF NOT EXISTS `game` (
   `id` varchar(40) NOT NULL,
   `location` varchar(10) DEFAULT NULL,
   `screen_name` varchar(40) DEFAULT NULL,
@@ -81,53 +64,29 @@ CREATE TABLE `game` (
   CONSTRAINT `fk_location_airport` FOREIGN KEY (`location`) REFERENCES `airport` (`ident`),
   CONSTRAINT `game_ibfk_1` FOREIGN KEY (`location`) REFERENCES `airport` (`ident`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `game`
---
+-- Dumping data for table flight_game.game: ~3 rows (approximately)
+REPLACE INTO `game` (`id`, `location`, `screen_name`, `fuel`, `war_points`) VALUES
+	('1', 'EFHK', 'Heini', 100, 0),
+	('2', 'EGCC', 'Vesa', 500, 0),
+	('3', 'EGKK', 'Ilkka', 500, 0);
 
-LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-INSERT INTO `game` VALUES
-('1','EFHK','Heini',500,0),
-('2','EGCC','Vesa',500,0),
-('3','EGKK','Ilkka',500,0);
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `store`
---
-
-DROP TABLE IF EXISTS `store`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `store` (
+-- Dumping structure for table flight_game.store
+CREATE TABLE IF NOT EXISTS `store` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item` varchar(100) NOT NULL,
   `effect` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `store`
---
+-- Dumping data for table flight_game.store: ~2 rows (approximately)
+REPLACE INTO `store` (`id`, `item`, `effect`, `price`) VALUES
+	(1, 'moottori', 'Tekee bensan käytöstä tehokkaampaa', 1000),
+	(2, 'moottori prime', 'Tekee bensan käytöstä vielä tehokkaampaa', 2000);
 
-LOCK TABLES `store` WRITE;
-/*!40000 ALTER TABLE `store` DISABLE KEYS */;
-/*!40000 ALTER TABLE `store` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
-
--- Dump completed on 2024-09-30 13:55:46
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
